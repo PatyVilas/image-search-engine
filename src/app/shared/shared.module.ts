b75f3db76612648d441f68b63b378383d9cb40ba
PatyVilas <81257264+PatyVilas@users.noreply.github.com>
2021-11-16 16:13:54 +0100

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+
+
+
+@NgModule({
+  declarations: [
+    TopBarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
